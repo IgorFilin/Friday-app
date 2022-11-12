6eaa4c8b3e6b854662098552562f36a871260cf6
Samurai-way <olegmoishevych@gmail.com>
2022-11-12 22:12:35 +0200

    build: writing login component with MUI

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {App} from "./app/App";
 
-import { App } from './app/App';
-import { store } from './redux/store';
+
+
+
+// import {store} from "./redux/store";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
+    {/*<Provider store={store}>*/}
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </Provider>
+    {/*</Provider>*/}
   </BrowserRouter>,
 );
 
