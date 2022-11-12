5c856612c3eef63e5b5125d137f1f68630518454
SPK80 <psukonkin@gmail.com>
2022-11-12 16:44:16 +0300

    feat: create Profile layout (part-1)

--- src/feature/profile/Profile.tsx
+++ src/feature/profile/Profile.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from 'react'
+import Paper from '@mui/material/Paper'
+import Container from '@mui/material/Container'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 
 export const Profile = () => {
     return (
-        <div>
-            profile
-        </div>
-    );
-};
-
+        <Container
+            maxWidth="sm"
+            sx={{
+                display: 'flex',
+                height: '100vh',
+                justifyContent: 'center',
+            }}
+        >
+            <Paper
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    mt: 7,
+                    width: 400,
+                    height: 300,
+                }}
+            >
+                <Stack sx={{ m: 3 }}>
+                    <Typography variant="h5">Personal Information</Typography>
+                </Stack>
+            </Paper>
+        </Container>
+    )
+}
