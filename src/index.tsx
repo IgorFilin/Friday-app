2c86908d65b86e7fa08fcb78b844e5eac4519b82
Igor <filinigor@yandex.ru>
2022-11-13 11:06:22 +0300

    fix: fix problems imports in App

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,21 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./app/App";
+import { store } from "./redux/store";
 
-import ReactDOM from 'react-dom/client';
-
-import './index.css';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-import {App} from "./app/App";
-
-
-
-
-// import {store} from "./redux/store";
-
-import { App } from './App';
-import { store } from './redux/store';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <BrowserRouter>
-    {/*<Provider store={store}>*/}
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    {/*</Provider>*/}
-  </BrowserRouter>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
