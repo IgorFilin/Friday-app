5dcddf20afd6a110c6cd2fb2c573478857d9d3f4
SPK80 <psukonkin@gmail.com>
2022-11-14 09:49:12 +0300

    refactor: remove React.StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -16,9 +16,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <App />
     </Provider>
   </BrowserRouter>
 );
