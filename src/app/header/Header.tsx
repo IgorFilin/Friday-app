3c9c6a8cb7c61cbd5527aaade9957d1ad412f65e
SPK80 <psukonkin@gmail.com>
2022-11-15 09:06:31 +0300

    feat: create HeaderAvatar and refactor Header

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,41 +1,47 @@
-import React from "react";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import { Navigate, useNavigate } from "react-router-dom";
+import React from 'react'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import { Button } from '@mui/material'
+import AppBar from '@mui/material/AppBar'
+import { useNavigate } from 'react-router-dom'
+import { useAppSelector } from 'redux/store'
+import { HeaderAvatar } from './HeaderAvatar'
 
 export const Header = () => {
-  let navigate = useNavigate();
+    const isLogin = useAppSelector((state) => state.auth.isLogin)
 
-  const onClickSingInHandler = () => {
-    navigate("/login");
-  };
+    let navigate = useNavigate()
+    const onClickSingInHandler = () => {
+        navigate('/login')
+    }
 
-  return (
-    <AppBar color={"inherit"} position="static">
-      <Toolbar>
-        <img
-          style={{ marginLeft: "10%" }}
-          src={
-            "https://it-incubator.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.8a063c2a.svg&w=256&q=75"
-          }
-        />
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          {/*News*/}
-        </Typography>
-        <Button
-          sx={{
-            mr: "10%",
-            width: "113px",
-            borderRadius: 5,
-          }}
-          variant="contained"
-          onClick={onClickSingInHandler}
-        >
-          Sign in
-        </Button>
-      </Toolbar>
-    </AppBar>
-  );
-};
+    return (
+        <AppBar color={'inherit'} position="static" sx={{ pl: '10%', pr: '10%' }}>
+            <Toolbar>
+                <img
+                    src={
+                        'https://it-incubator.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.8a063c2a.svg&w=256&q=75'
+                    }
+                    alt={'logo'}
+                />
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    {/*News*/}
+                </Typography>
+                {isLogin ? (
+                    <HeaderAvatar />
+                ) : (
+                    <Button
+                        sx={{
+                            width: '113px',
+                            borderRadius: 5,
+                        }}
+                        variant="contained"
+                        onClick={onClickSingInHandler}
+                    >
+                        Sign in
+                    </Button>
+                )}
+            </Toolbar>
+        </AppBar>
+    )
+}
