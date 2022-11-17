24146dabe03d01d8c0b87522d8938994894d9893
SPK80 <psukonkin@gmail.com>
2022-11-17 06:07:02 +0300

    refactor: change InputSearch

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -1,36 +1,35 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import { InputAdornment } from "@mui/material";
-import SearchSharpIcon from "@mui/icons-material/SearchSharp";
-import Box from "@mui/material/Box";
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import { InputAdornment } from '@mui/material'
+import SearchSharpIcon from '@mui/icons-material/SearchSharp'
+import Box from '@mui/material/Box'
 
-export const InputSearch = () => {
-  const [inputValue, setInputValue] = React.useState("");
-  return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "5px",
-        }}
-      >
-        <Typography variant="h6">Search</Typography>
-        <TextField
-          size={"small"}
-          placeholder={"Provide your text"}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.currentTarget.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchSharpIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Box>
-    </>
-  );
-};
+export const InputSearch: React.FC<{ width?: number | string }> = ({ width }) => {
+    const [inputValue, setInputValue] = React.useState('')
+    return (
+        <Box
+            sx={{
+                width,
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '5px',
+            }}
+        >
+            <Typography variant="h6">Search</Typography>
+            <TextField
+                size={'small'}
+                placeholder={'Provide your text'}
+                value={inputValue}
+                onChange={(e) => setInputValue(e.currentTarget.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchSharpIcon />
+                        </InputAdornment>
+                    ),
+                }}
+            />
+        </Box>
+    )
+}
