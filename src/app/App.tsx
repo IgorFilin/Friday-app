9b7cb0b984d32b90ec43ba9b9ac948db880711a6
Samurai-way <olegmoishevych@gmail.com>
2022-11-18 16:45:21 +0200

    fix: NewPassword, app, api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,32 @@
-import React, { useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from 'redux/store'
-import { initializeAppTC, RequestStatus } from 'redux/app-reducer'
-import { Header } from './header/Header'
-import { ErrorSnackbar } from 'components/ErrorSnackbar'
-import { InfoSnackbar } from 'components/InfoSnackbar'
-import { AppCircularProgress } from './AppCircularProgress'
-import { AppRoutes } from './AppRoutes'
-import Box from '@mui/material/Box'
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "redux/store";
+import { initializeAppTC, RequestStatus } from "redux/app-reducer";
+import { Header } from "./header/Header";
+import { ErrorSnackbar } from "components/ErrorSnackbar";
+import { InfoSnackbar } from "components/InfoSnackbar";
+import { AppCircularProgress } from "./AppCircularProgress";
+import { AppRoutes } from "./AppRoutes";
+import Box from "@mui/material/Box";
 
 export const App: React.FC = () => {
-    const requestStatus = useAppSelector((state) => state.app.request.status)
-    const isInitialized = useAppSelector((state) => state.app.isInitialized)
-    const dispatch = useAppDispatch()
+  const requestStatus = useAppSelector((state) => state.app.request.status);
+  const isInitialized = useAppSelector((state) => state.app.isInitialized);
 
-    useEffect(() => {
-        dispatch(initializeAppTC())
-    }, [])
+  const dispatch = useAppDispatch();
 
-    if (!isInitialized) return <AppCircularProgress />
+  useEffect(() => {
+    dispatch(initializeAppTC());
+  }, []);
 
-    return (
-        <Box>
-            <Header />
-            {requestStatus === RequestStatus.loading && <AppCircularProgress />}
-            <AppRoutes />
-            <ErrorSnackbar />
-            <InfoSnackbar />
-        </Box>
-    )
-}
+  if (!isInitialized) return <AppCircularProgress />;
+
+  return (
+    <Box>
+      <Header />
+      {requestStatus === RequestStatus.loading && <AppCircularProgress />}
+      <AppRoutes />
+      <ErrorSnackbar />
+      <InfoSnackbar />
+    </Box>
+  );
+};
