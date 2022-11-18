f3982af45cef4fc2d79e40721a17b51a8716151d
Samurai-way <olegmoishevych@gmail.com>
2022-11-18 16:53:59 +0200

    fix: CheckEmail

--- src/feature/password_recovery/CheckEmail.tsx
+++ src/feature/password_recovery/CheckEmail.tsx
@@ -1,34 +1,45 @@
-import React from 'react';
+import React from "react";
 import s from "./CheckEmail.module.css";
 import { Button, Paper } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import image from "assets/CheckEmail.png"
+import image from "assets/CheckEmail.png";
 import { useSelector } from "react-redux";
 import { AppRootReducerType } from "../../redux/store";
 
 export const CheckEmail = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleClickToLogin = () => {
+    navigate("/login");
+  };
+  const email = useSelector<AppRootReducerType, string>(
+    (state) => state.auth.email
+  );
+  const statusLoading = useSelector<AppRootReducerType, RequestStatus>(
+    (state) => state.app.request.status
+  );
 
-    const handleClickToLogin = () => {
-        navigate("/login");
-    }
-    const email = useSelector<AppRootReducerType, string>(
-        (state) => state.auth.email
-    );
-
-    return (
+  return (
+    <>
+      {!(statusLoading === RequestStatus.loading) && (
         <div className={s.checkEmailWrapper}>
-            <Paper className={s.checkEmail} elevation={3}>
-                <h2 className={s.h2}>Check Email</h2>
-                <img src={image} className={s.img} alt="check email" />
-                <p className={s.p}>
-                    We've sent an Email with instructions to <br />
-                    {email}
-                </p>
-                <Button className={s.button} onClick={handleClickToLogin} title={"Back to login"}>Back to login</Button>
-            </Paper>
+          <Paper className={s.checkEmail} elevation={3}>
+            <h2 className={s.h2}>Check Email</h2>
+            <img src={image} className={s.img} alt="check email" />
+            <p className={s.p}>
+              We've sent an Email with instructions to <br />
+              {email}
+            </p>
+            <Button
+              className={s.button}
+              onClick={handleClickToLogin}
+              title={"Back to login"}
+            >
+              Back to login
+            </Button>
+          </Paper>
         </div>
-    );
+      )}
+    </>
+  );
 };
-
