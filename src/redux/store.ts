06b0099bfbe4afa2c5c421f9651112db3d30f93b
Igor <filinigor@yandex.ru>
2022-11-19 12:18:25 +0300

    feat: add in store packsCardReducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,30 +1,27 @@
-import {
-  applyMiddleware,
-  combineReducers,
-  legacy_createStore as createStore,
-} from "redux";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import thunk, { ThunkDispatch } from "redux-thunk";
-import { storeReducer } from "./storeReducer";
-import { appReducer } from "./app-reducer";
-import { authReducer } from "./auth-reducer";
+import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import thunk, { ThunkDispatch } from 'redux-thunk'
+import { storeReducer } from './storeReducer'
+import { appReducer } from './app-reducer'
+import { authReducer } from './auth-reducer'
+import { packsCardReducer } from './packs-reducer'
 
 const rootReducer = combineReducers({
-  store: storeReducer,
-  app: appReducer,
-  auth: authReducer,
-});
+    store: storeReducer,
+    app: appReducer,
+    auth: authReducer,
+    packsCard: packsCardReducer,
+})
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(rootReducer, applyMiddleware(thunk))
 
-export type AppRootReducerType = ReturnType<typeof rootReducer>;
+export type AppRootReducerType = ReturnType<typeof rootReducer>
 
-export type AppDispatch = ThunkDispatch<AppRootReducerType, unknown, any>;
+export type AppDispatch = ThunkDispatch<AppRootReducerType, unknown, any>
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<AppRootReducerType> =
-  useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<AppRootReducerType> = useSelector
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
-window.store = store;
+window.store = store
