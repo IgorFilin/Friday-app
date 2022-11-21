904d3876408a386356f6a7f68abe3b1e4972f34b
SPK80 <psukonkin@gmail.com>
2022-11-21 15:21:39 +0300

    feat: change cards state, implement updateCardAC, createCardTC, deleteCardTC, updateCardTC

--- src/redux/cardsReducer.ts
+++ src/redux/cardsReducer.ts
@@ -1,24 +1,23 @@
-import { cardsApi, CardsStateType, GetCardsParamsType } from 'api/api'
+import { cardsApi, CardsStateType, CardType, GetCardsParamsType, NewCardType } from 'api/api'
 import { Dispatch } from 'redux'
 import { RequestStatus, setErrorAC, setLoadingAC } from './appReducer'
 
 //===TYPES======================================================================
 
-type AuthActionsType = ReturnType<typeof setCardsAC>
+type AuthActionsType = ReturnType<typeof setCardsAC> | ReturnType<typeof updateCardAC>
+
 type InitialStateType = typeof initialState
 
 //===REDUCER====================================================================
 
 const initialState = {
-    cardsState: {
-        cards: [],
-        cardsTotalCount: 0,
-        page: 1,
-        pageCount: 0,
-        maxGrade: 0,
-        minGrade: 0,
-        packUserId: '',
-    } as CardsStateType,
+    cards: [] as Array<CardType>,
+    cardsTotalCount: 0,
+    page: 1,
+    pageCount: 0,
+    maxGrade: 0,
+    minGrade: 0,
+    packUserId: '',
 }
 
 export const cardsReducer = (
@@ -27,7 +26,14 @@ export const cardsReducer = (
 ): InitialStateType => {
     switch (action.type) {
         case 'CARDS/SET-CARDS':
-            return { ...state, cardsState: action.cardsState }
+            return { ...state, ...action.cardsState }
+        case 'CARDS/UPDATE-CARD':
+            return {
+                ...state,
+                cards: state.cards.map((card) =>
+                    card._id === action.card._id ? { ...card, ...action.card } : card
+                ),
+            }
         default: {
             return state
         }
@@ -40,6 +46,10 @@ export const setCardsAC = (cardsState: CardsStateType) => {
     return { type: 'CARDS/SET-CARDS', cardsState } as const
 }
 
+export const updateCardAC = (card: CardType) => {
+    return { type: 'CARDS/UPDATE-CARD', card } as const
+}
+
 //===THUNKS=====================================================================
 
 export const fetchCardsTC = (params: GetCardsParamsType) => async (dispatch: Dispatch) => {
@@ -47,6 +57,43 @@ export const fetchCardsTC = (params: GetCardsParamsType) => async (dispatch: Dis
         dispatch(setLoadingAC(RequestStatus.loading))
         const res = await cardsApi.getCards(params)
         dispatch(setCardsAC(res))
+    } catch (error) {
+        dispatch(setErrorAC(error as string))
+        dispatch(setLoadingAC(RequestStatus.error))
+    } finally {
+        dispatch(setLoadingAC(RequestStatus.idle))
+    }
+}
+
+export const createCardTC = (newCard: NewCardType) => async (dispatch: Dispatch) => {
+    try {
+        dispatch(setLoadingAC(RequestStatus.loading))
+        await cardsApi.createCard(newCard)
+    } catch (error) {
+        dispatch(setErrorAC(error as string))
+        dispatch(setLoadingAC(RequestStatus.error))
+    } finally {
+        dispatch(setLoadingAC(RequestStatus.idle))
+    }
+}
+
+export const deleteCardTC = (cardId: string) => async (dispatch: Dispatch) => {
+    try {
+        dispatch(setLoadingAC(RequestStatus.loading))
+        await cardsApi.deleteCard(cardId)
+    } catch (error) {
+        dispatch(setErrorAC(error as string))
+        dispatch(setLoadingAC(RequestStatus.error))
+    } finally {
+        dispatch(setLoadingAC(RequestStatus.idle))
+    }
+}
+
+export const updateCardTC = (card: CardType) => async (dispatch: Dispatch) => {
+    try {
+        dispatch(setLoadingAC(RequestStatus.loading))
+        const updatedCard = await cardsApi.updateCard(card)
+        dispatch(updateCardAC(updatedCard))
     } catch (error) {
         dispatch(setErrorAC(error as string))
         dispatch(setLoadingAC(RequestStatus.error))
