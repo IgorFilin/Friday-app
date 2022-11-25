6e7e6a3cccbc80dbbeee6e124e0f5c73800aa5e2
Igor <filinigor@yandex.ru>
2022-11-25 19:01:07 +0300

    fix: commit

--- src/feature/friendsPack/FriendsPack.tsx
+++ src/feature/friendsPack/FriendsPack.tsx
@@ -1,19 +1,19 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import KeyboardReturnRoundedIcon from '@mui/icons-material/KeyboardReturnRounded'
 import Box from '@mui/material/Box'
-import {Link, Navigate, useParams} from 'react-router-dom'
+import { Link, Navigate, useParams } from 'react-router-dom'
 import Typography from '@mui/material/Typography'
-import {TableComponent} from './TableComponent'
+import { TableComponent } from './TableComponent'
 import Stack from '@mui/material/Stack'
-import {TablePaginationComponent} from '../../components/TablePaginationComponent'
-import {InputSearch} from '../../components/InputSearch'
+import { TablePaginationComponent } from '../../components/TablePaginationComponent'
+import { InputSearch } from '../../components/InputSearch'
 import Container from '@mui/material/Container'
-import {BlueButton} from '../../components/BlueButton'
-import {useAppDispatch, useAppSelector} from '../../redux/store'
-import {setCardsTC} from '../../redux/decksReducer'
-import {AddToPack} from './AddPoPack'
-import {RequestStatus} from "../../redux/appReducer";
-import {LinearProgress} from "@mui/material";
+import { BlueButton } from '../../components/BlueButton'
+import { useAppDispatch, useAppSelector } from '../../redux/store'
+import { setCardsTC } from '../../redux/decksReducer'
+import { AddToPack } from './AddPoPack'
+import { RequestStatus } from '../../redux/appReducer'
+import { LinearProgress } from '@mui/material'
 
 export const FriendsPack = () => {
     const dispatch = useAppDispatch()
@@ -24,58 +24,68 @@ export const FriendsPack = () => {
     const pageCount = useAppSelector((state) => state.decks.cardsState.pageCount)
     const statusLoading = useAppSelector((state) => state.app.request.status)
 
-    const {id} = useParams<'id'>()
+    const { id } = useParams<'id'>()
 
     useEffect(() => {
         id && dispatch(setCardsTC(id, ''))
     }, [sort, pageCount, page])
 
     if (!isLogin) {
-        return <Navigate to={'/login'}/>
+        return <Navigate to={'/login'} />
     }
     const handleRequest = (param: string) => {
         id && dispatch(setCardsTC(id, param))
     }
 
     return (
-        <>{statusLoading === RequestStatus.loading ? (
-            <LinearProgress />
-        ) : (
-            <>
-                {!decks.cards.length ? (
-                    <AddToPack/>
-                ) : (
-                    <Container style={{maxWidth: '1000px'}}>
-                        <Box style={{width: '100%', margin: '24px auto'}}>
-                            <Link to={'/packslist'} style={{textDecoration: 'none', color: 'black'}}>
-                                <KeyboardReturnRoundedIcon sx={{mt: 2}}/> Back to Packs List
-                            </Link>
-                        </Box>
-                        <Box
-                            style={{
-                                width: '100%',
-                            }}
-                        >
-                            <Typography
-                                variant={'h6'}
+        <>
+            {statusLoading === RequestStatus.loading ? (
+                <LinearProgress />
+            ) : (
+                <>
+                    {!decks.cards.length ? (
+                        <AddToPack />
+                    ) : (
+                        <Container style={{ maxWidth: '1000px' }}>
+                            <Box style={{ width: '100%', margin: '24px auto' }}>
+                                <Link
+                                    to={'/packslist'}
+                                    style={{ textDecoration: 'none', color: 'black' }}
+                                >
+                                    <KeyboardReturnRoundedIcon sx={{ mt: 2 }} /> Back to Packs List
+                                </Link>
+                            </Box>
+                            <Box
                                 style={{
-                                    fontWeight: 'bold',
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    marginTop: '27px',
+                                    width: '100%',
                                 }}
                             >
-                                Friend’s Pack
-                                <BlueButton onClick={()=>{alert('in progress')}}>Learn to pack</BlueButton>
-                            </Typography>
-                        </Box>
-                        <InputSearch handleRequest={handleRequest}/>
-                        <TableComponent/>
-                        <Stack sx={{width: '100%', margin: '0 auto'}} spacing={2}></Stack>
-                        <TablePaginationComponent/>
-                    </Container>
-                )}
-            </>
+                                <Typography
+                                    variant={'h6'}
+                                    style={{
+                                        fontWeight: 'bold',
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        marginTop: '27px',
+                                    }}
+                                >
+                                    Friend’s Pack
+                                    <BlueButton
+                                        onClick={() => {
+                                            alert('in progress')
+                                        }}
+                                    >
+                                        Learn to pack
+                                    </BlueButton>
+                                </Typography>
+                            </Box>
+                            <InputSearch handleRequest={handleRequest} />
+                            <TableComponent />
+                            <Stack sx={{ width: '100%', margin: '0 auto' }} spacing={2}></Stack>
+                            <TablePaginationComponent />
+                        </Container>
+                    )}
+                </>
             )}
         </>
     )
