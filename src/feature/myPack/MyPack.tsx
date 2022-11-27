d9ed2db9fc79e5a449dd479c38c27cecd5fb8262
SPK80 <psukonkin@gmail.com>
2022-11-27 16:58:20 +0300

    refactor: clean and optimise code in MyPack

--- src/feature/myPack/MyPack.tsx
+++ src/feature/myPack/MyPack.tsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { useAppDispatch, useAppSelector } from 'redux/store'
-import { Navigate, useParams } from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import {useAppDispatch, useAppSelector} from 'redux/store'
+import {Navigate, useParams} from 'react-router-dom'
 import Container from '@mui/material/Container'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import { Path } from 'app/AppRoutes'
-import { BackToPacksListButton } from 'components/BackToPacksListButton'
-import { PrimaryButton } from 'components/PrimaryButton'
-import { CardsTable } from 'components/CardsTable'
-import { CardsPagination } from 'components/CardsPagination'
-import { MyPackButtonWithMenu } from './MyPackButtonWithMenu'
-import { fetchCardsTC } from 'redux/cardsReducer'
-import { setErrorAC } from 'redux/appReducer'
-import { CardsSearchInput } from 'components/CardsSearchInput'
-import { AddNewCardDialog } from 'components/AddNewCardDialog'
+import {Path} from 'app/AppRoutes'
+import {BackToPacksListButton} from 'components/BackToPacksListButton'
+import {PrimaryButton} from 'components/PrimaryButton'
+import {CardsTable} from 'components/CardsTable'
+import {CardsPagination} from 'components/CardsPagination'
+import {MyPackButtonWithMenu} from './MyPackButtonWithMenu'
+import {fetchCardsTC} from 'redux/cardsReducer'
+import {setErrorAC} from 'redux/appReducer'
+import {CardsSearchInput} from 'components/CardsSearchInput'
+import {AddNewCardDialog} from 'components/AddNewCardDialog'
 
 export const MyPack: React.FC = () => {
     const userId = useAppSelector((state) => state.auth.profileData.id)
     const cardsState = useAppSelector((state) => state.cards)
     const dispatch = useAppDispatch()
     const [isNewCardModalOpen, setIsNewCardModalOpen] = useState(false)
-
-    const { packId } = useParams<'packId'>()
-
+    
+    const {packId} = useParams<'packId'>()
+    
     useEffect(() => {
         if (packId) dispatch(fetchCardsTC(packId))
     }, [
@@ -34,18 +34,24 @@ export const MyPack: React.FC = () => {
         packId,
         dispatch,
     ])
+    
     const onAddCardClickHandler = () => {
         setIsNewCardModalOpen(true)
-        // if (!packId) return
-        // dispatch(
-        //     createCardTC({ cardsPack_id: packId, answer: 'test answer', question: 'test question' })
-        // )
     }
+    
     if (cardsState.packUserId !== '' && cardsState.packUserId !== userId) {
         dispatch(setErrorAC("It's not yours Cards Pack"))
-        return <Navigate to={Path.packsList} />
+        return <Navigate to={Path.packsList}/>
     }
-
+    
+    const rows = cardsState.cards.map((c) => ({
+        id: c._id,
+        question: c.question,
+        answer: c.answer,
+        lastUpdated: c.updated,
+        grade: c.grade,
+    }))
+    
     return (
         <Container
             sx={{
@@ -58,9 +64,9 @@ export const MyPack: React.FC = () => {
                 onClose={() => setIsNewCardModalOpen(false)}
                 open={isNewCardModalOpen}
             />
-            <Stack width={'100%'} sx={{ m: 3, alignItems: 'center' }}>
+            <Stack width={'100%'} sx={{m: 3, alignItems: 'center'}}>
                 <Box width={'100%'} marginBottom={2}>
-                    <BackToPacksListButton />
+                    <BackToPacksListButton/>
                 </Box>
                 <Box
                     display={'flex'}
@@ -73,29 +79,18 @@ export const MyPack: React.FC = () => {
                         <Typography component="span" variant={'h5'}>
                             My Pack
                         </Typography>
-                        <MyPackButtonWithMenu />
+                        <MyPackButtonWithMenu/>
                     </Box>
                     <PrimaryButton onClick={onAddCardClickHandler}>Add new card</PrimaryButton>
                 </Box>
-                <CardsSearchInput width={'100%'} />
-                <br />
-                {packId && packId !== '' ? (
+                <CardsSearchInput width={'100%'}/>
+                <br/>
+                {packId && packId !== '' &&
                     <>
-                        <CardsTable
-                            packId={packId}
-                            rows={cardsState.cards.map((c) => ({
-                                id: c._id,
-                                question: c.question,
-                                answer: c.answer,
-                                lastUpdated: c.updated,
-                                grade: c.grade,
-                            }))}
-                        />
-                        <CardsPagination />
+                        <CardsTable packId={packId} rows={rows}/>
+                        <CardsPagination/>
                     </>
-                ) : (
-                    <></>
-                )}
+                }
             </Stack>
         </Container>
     )
